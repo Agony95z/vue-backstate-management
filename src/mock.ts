d83e1bdac564a023e427b29510690303f6613af4
张卫海 <zhangweihai@zichan360.com>
2023-03-08 22:34:19 +0800

    feat: add mock data

---
+++ src/mock.ts
@@ -0,0 +1,31 @@
+const info = {
+  name: "黄蓉",
+  permission: ["home", "sign", "exception", "apply"],
+  _id: "62632f3f674b1e20c841aae2",
+  approver: [{ _id: "626c7236e0c7edf6ce507708", name: "洪七公" }],
+  head: "http://api.h5ke.top/uploads/62632f3f674b1e20c841aae2.png",
+};
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbmZvcyI6eyJuYW1lIjoi6buE6JOJIiwicGVybWlzc2lvbiI6WyJob21lIiwic2lnbiIsImV4Y2VwdGlvbiIsImFwcGx5Il0sIl9pZCI6IjYyNjMyZjNmNjc0YjFlMjBjODQxYWFlMiIsImFwcHJvdmVyIjpbeyJfaWQiOiI2MjZjNzIzNmUwYzdlZGY2Y2U1MDc3MDgiLCJuYW1lIjoi5rSq5LiD5YWsIn1dLCJoZWFkIjoiaHR0cDovL2FwaS5oNWtlLnRvcC91cGxvYWRzLzYyNjMyZjNmNjc0YjFlMjBjODQxYWFlMi5wbmcifSwiaWF0IjoxNjc4Mjg1Mzg2LCJleHAiOjE2NzgyODg5ODZ9.yJaHB5KTuohQ4iGrj-lqYURBBoL9YNvv0brH1DS8ikM";
+
+// login
+const loginData = {
+  errcode: 0,
+  errmsg: "ok",
+  token:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbmZvcyI6eyJuYW1lIjoi6buE6JOJIiwicGVybWlzc2lvbiI6WyJob21lIiwic2lnbiIsImV4Y2VwdGlvbiIsImFwcGx5Il0sIl9pZCI6IjYyNjMyZjNmNjc0YjFlMjBjODQxYWFlMiIsImFwcHJvdmVyIjpbeyJfaWQiOiI2MjZjNzIzNmUwYzdlZGY2Y2U1MDc3MDgiLCJuYW1lIjoi5rSq5LiD5YWsIn1dLCJoZWFkIjoiaHR0cDovL2FwaS5oNWtlLnRvcC91cGxvYWRzLzYyNjMyZjNmNjc0YjFlMjBjODQxYWFlMi5wbmcifSwiaWF0IjoxNjc4Mjg1Mzg2LCJleHAiOjE2NzgyODg5ODZ9.yJaHB5KTuohQ4iGrj-lqYURBBoL9YNvv0brH1DS8ikM",
+};
+
+// infos
+const infosData = {
+  errcode: 0,
+  errmsg: "infos success",
+  infos: {
+    name: "黄蓉",
+    permission: ["home", "sign", "exception", "apply"],
+    _id: "62632f3f674b1e20c841aae2",
+    approver: [{ _id: "626c7236e0c7edf6ce507708", name: "洪七公" }],
+    head: "http://api.h5ke.top/uploads/62632f3f674b1e20c841aae2.png",
+  },
+};
+export {};
